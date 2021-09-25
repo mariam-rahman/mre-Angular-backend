b41bafea211fbe3f64f5b8b3ac96dcdb7d9a54a2
suhrab <suhrabahdi@gmail.com>
2021-09-25 11:28:53 -0700

    authorization

--- resources/views/livewire/user/user-component.blade.php
+++ resources/views/livewire/user/user-component.blade.php
@@ -1,13 +1,7 @@
 <div>
     <form wire:submit.prevent>
         <div class="form-group">
-            <div>
-                @if(session()->has('message'))
-                <script>
-                    toastr.success("{{session()->get('message')}}");
-                </script>
-                @endif
-            </div>
+        
             <label><strong>Email</strong></label>
             <input type="email" wire:model='email' class="form-control">
             @error('email') <span class="text-danger">{{ $message }}</span> @enderror
